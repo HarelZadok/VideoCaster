9b7f363d58b7522cca13615cca32f027a9ab0813
Harel <harelzad@gmail.com>
2024-11-20 04:23:31 +0200

    Initial Commit

---
+++ VideoCaster/VideoCasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoCasterApp.swift
+//  VideoCaster
+//
+//  Created by Harel Zadok on 20/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct VideoCasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VideoCasterTests/VideoCasterTests.swift
@@ -0,0 +1,17 @@
+//
+//  VideoCasterTests.swift
+//  VideoCasterTests
+//
+//  Created by Harel Zadok on 20/11/2024.
+//
+
+import Testing
+@testable import VideoCaster
+
+struct VideoCasterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
